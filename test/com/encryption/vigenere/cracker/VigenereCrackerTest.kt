e39b296221c3a41dc17eb1991f0b1a725776c151
Bill Chesky <billchesky@yahoo.com>
2025-07-12 23:39:11 -0400

    Create test case representing current best performance.

--- test/com/encryption/vigenere/cracker/VigenereCrackerTest.kt
+++ test/com/encryption/vigenere/cracker/VigenereCrackerTest.kt
@@ -8,8 +8,12 @@ import java.io.File
 
 class VigenereCrackerTest {
     @Test
-    fun test1() {
-        val keys = listOf("SAM", "TOM", "BILL", "FRODO", "BILBO", "GIMLI", "ELROND", "LEGOLAS", "GANDALF", "ARAGORN", "BOROMIR", "DENETHOR", "GALADRIEL", "GLORFINDEL", "TOMTOMTOMSAM", "GANDALFTHEGREY","AAA", "TTTT", "XXXXX", "ABBA", "XY" )
+    fun currentBestCaseTest() {
+        val keys = listOf(
+            "SAM", "TOM", "BILL", "FRODO", "BILBO", "GIMLI", "ELROND", "LEGOLAS", "GANDALF", "ARAGORN", "BOROMIR",
+            "DENETHOR", "GALADRIEL", "GLORFINDEL", "TOMTOMTOMSAM", "GANDALFTHEGREY","AAA", "TTTT", "XXXXX", "ABBA",
+            "XY", "OFJHZLKGHOOGHIM"
+        )
         val testFileNames = listOf("tcoe.txt", "atotc.txt", "hobbit.txt")
 
         for (fileName in testFileNames) {
